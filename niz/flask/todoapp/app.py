ee43b3f3ab30b6caec9bb2d1804bfbebdd5a8374
Joon <jtj308@naver.com>
2020-10-10 13:15:43 +0900

    final change

--- niz/flask/todoapp/app.py
+++ niz/flask/todoapp/app.py
@@ -4,20 +4,18 @@ from extensions import mongo
 from dotenv import load_dotenv
 from main.routes import main
 
-
 load_dotenv()
 user_id = os.getenv('USER_ID')
 user_password = os.getenv('USER_PASSWORD')
 
 app = Flask(__name__)
-
 # Need dnspython
 app.config['MONGO_URI'] = f'mongodb+srv://{user_id}:{user_password}@cluster0.hbauo.mongodb.net/testdb?retryWrites=true&w=majority'
+# mongodb+srv://<username>:<password>@cluster0.hbauo.mongodb.net/<dbname>?retryWrites=true&w=majority
 mongo.init_app(app)
 todos = mongo.db.todos
 
 app.register_blueprint(main)
-
 
 
 
